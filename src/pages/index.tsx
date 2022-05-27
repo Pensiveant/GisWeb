09f7f4632eec8d0c5892d2739717ef251acea3ae
Pensiveant <1429055703@qq.com>
2022-05-27 10:21:10 +0800

    feat: 添加公共创建地图工具，LayerCreate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,39 @@ import ArcMapView from '@/components/ArcGis/ArcMapView';
 import ArcSceneView from '@/components/ArcGis/ArcSceneView';
 import Map from '@arcgis/core/Map';
 import { useState } from 'react';
+import LayerCreate from '@/utils/LayerCreate';
+import WebScene from '@arcgis/core/WebScene';
 
 esriConfig.assetsPath = './arcgis/assets';
 
 export default function IndexPage() {
   const [viewProps, setViewProps] = useState({
-    map: new Map({
-      basemap: 'streets-vector',
+    map: new WebScene({
+      portalItem: {
+        id: 'f477c289e93347aba6a0c052bfe0e0a4',
+      },
     }),
   });
 
   return (
     <div className={styles.indexPage}>
-      <ArcMapView viewProps={viewProps} />
+      <ArcSceneView
+        viewProps={viewProps}
+        onLoad={(view) => {
+          let layer = LayerCreate('scene', {
+            id: 'sumx',
+            elevationInfo: {
+              mode: 'absolute-height',
+              offset: 15,
+            },
+            title: '南方美谷方案模型',
+            url: 'https://tiles.arcgis.com/tiles/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Esri_Admin_Building/SceneServer',
+          });
+          if (layer) {
+            view.map.add(layer);
+          }
+        }}
+      />
     </div>
   );
 }
---
+++ src/utils/LayerCreate.ts
@@ -0,0 +1,60 @@
+/**
+ * @Description: 公共方法，用于创建图层
+ * @Author: Pensiveant
+ * @Date: 2022-05-27 09:34:36
+ */
+
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import TileLayer from '@arcgis/core/layers/TileLayer';
+import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
+import ImageryTileLayer from '@arcgis/core/layers/ImageryTileLayer';
+import MapImageLayer from '@arcgis/core/layers/MapImageLayer';
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
+import BuildingSceneLayer from '@arcgis/core/layers/BuildingSceneLayer';
+import SceneLayer from '@arcgis/core/layers/SceneLayer';
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+
+interface LayerFactory {
+  [type: string]: (props: any) => __esri.Layer;
+}
+
+let layerFactory: LayerFactory = {
+  feature: (props: __esri.FeatureLayerProperties) => {
+    return new FeatureLayer(props);
+  },
+  tile: (props: __esri.TileLayerProperties) => {
+    return new TileLayer(props);
+  },
+  'vector-tile': (props: __esri.VectorTileLayerProperties) => {
+    return new VectorTileLayer(props);
+  },
+  'imagery-tile': (props: __esri.ImageryTileLayerProperties) => {
+    return new ImageryTileLayer(props);
+  },
+  'map-image': (props: __esri.MapImageLayerProperties) => {
+    return new MapImageLayer(props);
+  },
+  geojson: (props: __esri.GeoJSONLayerProperties) => {
+    return new GeoJSONLayer(props);
+  },
+  graphics: (props: __esri.GraphicsLayerProperties) => {
+    return new GraphicsLayer(props);
+  },
+  'building-scene': (props: __esri.BuildingSceneLayerProperties) => {
+    return new BuildingSceneLayer(props);
+  },
+  scene: (props: __esri.SceneLayerProperties) => {
+    return new SceneLayer(props);
+  },
+};
+
+let LayerCreate = function (type: string, props: any) {
+  const types = Object.keys(layerFactory);
+  if (types.includes(type)) {
+    return layerFactory[type](props);
+  } else {
+    console.log('无类型图层创建函数，请自行扩展。');
+  }
+};
+
+export default LayerCreate;
