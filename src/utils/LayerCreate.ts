f25bfcdc931797a5f0f9776080099f15d5650f84
Pensiveant <1429055703@qq.com>
2022-09-29 14:41:53 +0800

    feat: LayerCreate兼容API所有图层

--- src/utils/LayerCreate.ts
+++ src/utils/LayerCreate.ts
@@ -15,6 +15,26 @@ import SceneLayer from '@arcgis/core/layers/SceneLayer';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
 import WebTileLayer from '@arcgis/core/layers/WebTileLayer';
 import IntegratedMeshLayer from '@arcgis/core/layers/IntegratedMeshLayer';
+import BingMapsLayer from '@arcgis/core/layers/BingMapsLayer';
+import CSVLayer from '@arcgis/core/layers/CSVLayer';
+import ElevationLayer from '@arcgis/core/layers/ElevationLayer';
+import GeoRSSLayer from '@arcgis/core/layers/GeoRSSLayer';
+import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
+import KMLLayer from '@arcgis/core/layers/KMLLayer';
+import LineOfSightLayer from '@arcgis/core/layers/LineOfSightLayer';
+import MediaLayer from '@arcgis/core/layers/MediaLayer';
+import OGCFeatureLayer from '@arcgis/core/layers/OGCFeatureLayer';
+import OpenStreetMapLayer from '@arcgis/core/layers/OpenStreetMapLayer';
+import PointCloudLayer from '@arcgis/core/layers/PointCloudLayer';
+import RouteLayer from '@arcgis/core/layers/RouteLayer';
+import StreamLayer from '@arcgis/core/layers/StreamLayer';
+import SubtypeGroupLayer from '@arcgis/core/layers/SubtypeGroupLayer';
+import VoxelLayer from '@arcgis/core/layers/VoxelLayer';
+import WCSLayer from '@arcgis/core/layers/WCSLayer';
+import WFSLayer from '@arcgis/core/layers/WFSLayer';
+import WMSLayer from '@arcgis/core/layers/WMSLayer';
+import WMTSLayer from '@arcgis/core/layers/WMTSLayer';
+
 interface LayerFactory {
   [type: string]: (props: any) => __esri.Layer;
 }
@@ -53,6 +73,63 @@ let layerFactory: LayerFactory = {
   'integrated-mesh': (props: __esri.IntegratedMeshLayerProperties) => {
     // 比如，加载倾斜摄影
     return new IntegratedMeshLayer(props);
+  },
+  'bing-maps': (props: __esri.BingMapsLayerProperties) => {
+    return new BingMapsLayer(props);
+  },
+  csv: (props: __esri.CSVLayerProperties) => {
+    return new CSVLayer(props);
+  },
+  elevation: (props: __esri.ElevationLayerProperties) => {
+    return new ElevationLayer(props);
+  },
+  'geo-rss': (props: __esri.GeoRSSLayerProperties) => {
+    return new GeoRSSLayer(props);
+  },
+  imagery: (props: __esri.ImageryLayerProperties) => {
+    return new ImageryLayer(props);
+  },
+  kml: (props: __esri.KMLLayerProperties) => {
+    return new KMLLayer(props);
+  },
+  'line-of-sight': (props: __esri.LineOfSightLayerProperties) => {
+    return new LineOfSightLayer(props);
+  },
+  media: (props: __esri.MediaLayerProperties) => {
+    return new MediaLayer(props);
+  },
+  'ogc-feature': (props: __esri.OGCFeatureLayerProperties) => {
+    return new OGCFeatureLayer(props);
+  },
+  'open-street-map': (props: __esri.OpenStreetMapLayerProperties) => {
+    return new OpenStreetMapLayer(props);
+  },
+  'point-cloud': (props: __esri.PointCloudLayerProperties) => {
+    return new PointCloudLayer(props);
+  },
+  route: (props: __esri.RouteLayerProperties) => {
+    return new RouteLayer(props);
+  },
+  stream: (props: __esri.StreamLayerProperties) => {
+    return new StreamLayer(props);
+  },
+  'subtype-group': (props: __esri.SubtypeGroupLayerProperties) => {
+    return new SubtypeGroupLayer(props);
+  },
+  voxel: (props: __esri.VoxelLayerProperties) => {
+    return new VoxelLayer(props);
+  },
+  wcs: (props: __esri.WCSLayerProperties) => {
+    return new WCSLayer(props);
+  },
+  wfs: (props: __esri.WFSLayerProperties) => {
+    return new WFSLayer(props);
+  },
+  wms: (props: __esri.WMSLayerProperties) => {
+    return new WMSLayer(props);
+  },
+  wmts: (props: __esri.WMTSLayerProperties) => {
+    return new WMTSLayer(props);
   },
 };
 
